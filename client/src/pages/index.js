6ccd1d5611b3252565975d44e48e9aaabb898519
Godwin Opara <godwinopara62@yahoo.com>
2023-07-14 19:22:58 +0100

    query track data and display on ui

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 /** importing our pages */
-import Tracks from './tracks';
+import Tracks from "./tracks";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+	uri: "http://localhost:4000",
+	cache: new InMemoryCache(),
+});
 
 export default function Pages() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Tracks />} path="/" />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<BrowserRouter>
+				<Routes>
+					<Route element={<Tracks />} path="/" />
+				</Routes>
+			</BrowserRouter>
+		</ApolloProvider>
+	);
 }
