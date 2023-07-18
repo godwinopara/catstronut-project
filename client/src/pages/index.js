ccd6f6b108212288073acdc82e0d619bf2f91d02
Godwin Opara <godwinopara62@yahoo.com>
2023-07-18 13:00:11 +0100

    added a page for showing each track details

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 /** importing our pages */
 import Tracks from "./tracks";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import Track from "./track";
 
 const client = new ApolloClient({
 	uri: "http://localhost:4000",
@@ -15,6 +16,7 @@ export default function Pages() {
 			<BrowserRouter>
 				<Routes>
 					<Route element={<Tracks />} path="/" />
+					<Route element={<Track />} path="/track/:trackId" />
 				</Routes>
 			</BrowserRouter>
 		</ApolloProvider>
